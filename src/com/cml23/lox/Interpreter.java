ca9f16611703f29caa6892a5d973ae23d72429c1
Cassidy Langenfeld <clang23231@gmail.com>
2022-07-20 23:21:57 -0400

    Begin implementing chapter "Functions"

--- src/com/cml23/lox/Interpreter.java
+++ src/com/cml23/lox/Interpreter.java
@@ -1,5 +1,7 @@
 package com.cml23.lox;
 
+import java.util.ArrayList;
+
 import com.cml23.lox.Expr.Assign;
 import com.cml23.lox.Expr.Binary;
 import com.cml23.lox.Expr.Grouping;
@@ -13,7 +15,29 @@ import com.cml23.lox.Stmt.Var;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-  private Environment environment = new Environment();
+  final Environment globals = new Environment();
+  private Environment environment = globals;
+
+  Interpreter() {
+    globals.define("clock", new LoxCallable() {
+      @Override
+      public int arity() {
+        return 0;
+      }
+
+      @Override
+      public Object call(Interpreter interpreter,
+          List<Object> arguments) {
+        return (double) System.currentTimeMillis() / 1000.0;
+      }
+
+      @Override
+      public String toString() {
+        return "<native fn>";
+      }
+    });
+
+  }
 
   void interpret(List<Stmt> statements) {
     try {
@@ -200,6 +224,29 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Object value = evaluate(expr.value);
     environment.assign(expr.name, value);
     return value;
+  }
+
+  @Override
+  public Object visitCallExpr(Expr.Call expr) {
+    Object callee = evaluate(expr.callee);
+
+    List<Object> arguments = new ArrayList<>();
+
+    for (Expr argument : expr.arguments) {
+      arguments.add(evaluate(argument));
+    }
+
+    if (!(callee instanceof LoxCallable)) {
+      throw new RuntimeError(expr.paren,
+          "Can only call functions and classes.");
+    }
+    LoxCallable function = (LoxCallable) callee;
+    if (arguments.size() != function.arity()) {
+      throw new RuntimeError(expr.paren, "Expected " +
+          function.arity() + " arguments but got " +
+          arguments.size() + ".");
+    }
+    return function.call(this, arguments);
   }
 
   @Override
---
+++ src/com/cml23/lox/LoxCallable.java
@@ -0,0 +1,9 @@
+package com.cml23.lox;
+
+import java.util.List;
+
+interface LoxCallable {
+  int arity();
+
+  Object call(Interpreter interpreter, List<Object> arguments);
+}
