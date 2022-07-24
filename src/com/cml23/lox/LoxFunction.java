a4728a03ece4e17ebe32ebe83a5220953e57e98a
Cassidy Langenfeld <clang23231@gmail.com>
2022-07-24 11:53:33 -0400

    Implement chapter "Classes"

--- src/com/cml23/lox/LoxFunction.java
+++ src/com/cml23/lox/LoxFunction.java
@@ -5,10 +5,19 @@ import java.util.List;
 public class LoxFunction implements LoxCallable {
   private final Stmt.Function declaration;
   private final Environment closure;
+  private final boolean isInitializer;
 
-  LoxFunction(Stmt.Function declaration, Environment closure) {
+  LoxFunction(Stmt.Function declaration, Environment closure,
+      boolean isInitializer) {
     this.declaration = declaration;
     this.closure = closure;
+    this.isInitializer = isInitializer;
+  }
+
+  LoxFunction bind(LoxInstance instance) {
+    Environment environment = new Environment(closure);
+    environment.define("this", instance);
+    return new LoxFunction(declaration, environment, isInitializer);
   }
 
   @Override
@@ -22,12 +31,15 @@ public class LoxFunction implements LoxCallable {
     for (int i = 0; i < declaration.params.size(); i++) {
       environment.define(declaration.params.get(i).lexeme, arguments.get(i));
     }
-
     try {
       interpreter.executeBlock(declaration.body, environment);
     } catch (Return returnValue) {
+      if (isInitializer)
+        return closure.getAt(0, "this");
       return returnValue.value;
     }
+    if (isInitializer)
+      return closure.getAt(0, "this");
     return null;
   }
 
--- src/com/cml23/lox/Parser.java
+++ src/com/cml23/lox/Parser.java
@@ -33,12 +33,30 @@ public class Parser {
     try {
       if (match(VAR))
         return varDeclaration();
+      if (match(FUN))
+        return function("function");
+      if (match(CLASS))
+        return classDeclaration();
 
       return statement();
     } catch (ParseError error) {
       synchronize();
       return null;
     }
+  }
+
+  private Stmt classDeclaration() {
+    Token name = consume(IDENTIFIER, "Expect class name.");
+    consume(LEFT_BRACE, "Expect '{'' before class body.");
+
+    List<Stmt.Function> methods = new ArrayList<>();
+    while (!check(RIGHT_BRACE) && !isAtEnd()) {
+      methods.add(function("method"));
+    }
+
+    consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+    return new Stmt.Class(name, methods);
   }
 
   private Stmt statement() {
@@ -52,8 +70,6 @@ public class Parser {
       return whileStatement();
     if (match(FOR))
       return forStatement();
-    if (match(FUN))
-      return function("function");
     if (match(RETURN))
       return returnStatement();
 
@@ -190,6 +206,9 @@ public class Parser {
       if (expr instanceof Expr.Variable) {
         Token name = ((Expr.Variable) expr).name;
         return new Expr.Assign(name, value);
+      } else if (expr instanceof Expr.Get) {
+        Expr.Get get = (Expr.Get) expr;
+        return new Expr.Set(get.object, get.name, value);
       }
       error(equals, "Invalid assignment target.");
     }
@@ -362,6 +381,10 @@ public class Parser {
     while (true) {
       if (match(LEFT_PAREN)) {
         expr = finishCall(expr);
+      } else if (match(DOT)) {
+        Token name = consume(IDENTIFIER,
+            "Expect property name after '.'");
+        expr = new Expr.Get(expr, name);
       } else {
         break;
       }
@@ -377,11 +400,12 @@ public class Parser {
       return new Expr.Literal(true);
     if (match(NIL))
       return new Expr.Literal(null);
-
     if (match(NUMBER, STRING)) {
       return new Expr.Literal(previous().literal);
     }
-
+    if (match(THIS)) {
+      return new Expr.This(previous());
+    }
     if (match(LEFT_PAREN)) {
       Expr expr = expression();
       consume(RIGHT_PAREN, "Expect ')' after expression.");
